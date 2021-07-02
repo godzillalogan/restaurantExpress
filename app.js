80f4987d7925048b32622e6bb74f40510d4e9250
godzillalogan <kenlogan0117@gmail.com>
2021-07-02 17:10:58 +0800

    2021.7.2 , 外 , S3 A3 : 打造餐廳清單  ,套入boostrap和CSS

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is restaurant built with Express')
+  res.render('index')
 })
+// setting static files
+app.use(express.static('public'))
 
 // start and listen on the Express server
 app.listen(port, () => {
