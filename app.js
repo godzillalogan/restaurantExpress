1600ca07ec9f44670564284a74a0a5c2cb4a98b1
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-07-03 13:07:18 +0800

    2021.7.3, 家, 餐廳首頁、SHOW都好了 剩搜尋功能

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // require packages used in the project
 const express = require('express')
 const app = express()
+// ./代表說在app.js同一層裡面找restaurant.json檔案
+const restaurantsList = require('./restaurant.json')
 const port = 3000
 
 // require express-handlebars here
+//沒有給 ./ ，代表去node_modules裡面去找
 const exphbs = require('express-handlebars')
 
 // setting template engine
@@ -13,8 +16,22 @@ app.set('view engine', 'handlebars')
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantsList.results})
 })
+
+//params, 用:代表變數
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  
+  //箭頭函式
+  const restaurant = restaurantsList.results.filter(restaurant => restaurant.id === Number(req.params.restaurant_id))
+
+  // const restaurant = restaurantsList.results.filter(function(restaurant){
+  //   //restaurant.id是數值，req.params.restaurant_id是字串
+  //   return restaurant.id === Number(req.params.restaurant_id)
+  // })
+  res.render('show', { restaurant: restaurant[0] })  //restauran是陣列，因此restaurant[0]
+})
+
 // setting static files
 app.use(express.static('public'))
 
